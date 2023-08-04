f5dff14c61b1aea1bfab76cc491058c40c2886c5
Mayuri Deore <deoremayuri39@gmail.com>
2023-08-04 21:31:55 +0530

    Banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Banner from './Components/Banner/Banner';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Banner/>
     </div>
   );
 }
